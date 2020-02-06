536d298e4ebc0af4257cd62026475f0a1e096a60
余建军 <dotx@yujianjundeMBP.lan>
2020-02-06 19:10:32 +0800

    城市名称匹配修正

--- nCov.js
+++ nCov.js
@@ -2,6 +2,7 @@ $(function(){
 
     var china ={};
 
+    // 根据数据匹配颜色
     var getColor = function(count){
         if (count > 5000)
             return '#420001'
@@ -38,6 +39,7 @@ $(function(){
         return '#FFFFFF'
     }
 
+    // 取得各省显示颜色
     var getColorByProvince = function(name_chn) {
         var pros = china.children
         for (var i = pros.length - 1; i >= 0; i--) {
@@ -47,40 +49,66 @@ $(function(){
         }
     }
 
-
-    var getColorByCity = function(city, cities) {
-        for (var i = cities.length - 1; i >= 0; i--) {
-            var name = cities[i].name == "恩施州" ? "恩施" : cities[i].name
-            name = cities[i].name == "湘西自治州" ? "湘西" : cities[i].name
-            name = cities[i].name == "黔南州" ? "黔南" : cities[i].name
-            name = cities[i].name == "黔东南州" ? "黔东南" : cities[i].name
-            name = cities[i].name == "黔西南州" ? "黔西南" : cities[i].name
-            name = cities[i].name == "阿坝州" ? "阿坝" : cities[i].name
-            name = cities[i].name == "海北州" ? "海北" : cities[i].name
-            name = cities[i].name == "甘南州" ? "甘南" : cities[i].name
-            name = cities[i].name == "伊犁州" ? "伊犁" : cities[i].name
-            name = cities[i].name == "兴安盟乌兰浩特" ? "兴安盟" : cities[i].name
-            name = cities[i].name == "德宏州" ? "德宏" : cities[i].name
-            name = cities[i].name == "文山州" ? "文山" : cities[i].name
-            name = cities[i].name == "楚雄州" ? "楚雄" : cities[i].name
-            name = cities[i].name == "西双版纳州" ? "西双版纳" : cities[i].name
-            name = cities[i].name == "昌江县" ? "昌江" : cities[i].name
-            name = cities[i].name == "琼中县" ? "琼中" : cities[i].name
-            name = cities[i].name == "保亭县" ? "保亭" : cities[i].name
-            name = cities[i].name == "陵水县" ? "陵水" : cities[i].name
-            if (city.indexOf(name) != -1) {
-                return getColor(cities[i]['total']['confirm'])
-            }
-        }
-        return '#FFFFFF'
+    // 处理直辖市和台湾省的下钻数据
+    var isMunicipality = function(name) {
+        var m = ['北京市', '上海市', '天津市', '重庆市', '台湾省']
+        if(m.indexOf(name)!=-1)
+            return true
+        return false
     }
 
-    var showProvince = function(adcode, province) {
+    // 腾讯数据和高德地图在地级市的名称方面有差异，匹配了部分
+    var correctCityName = function(city) {
+        var name = city == "恩施州" ? "恩施" : city
+        name = city == "湘西自治州" ? "湘西" : city
+        name = city == "黔南州" ? "黔南" : city
+        name = city == "黔东南州" ? "黔东南" : city
+        name = city == "黔西南州" ? "黔西南" : city
+        name = city == "阿坝州" ? "阿坝" : city
+        name = city == "海北州" ? "海北" : city
+        name = city == "海南州" ? "海南" : city
+        name = city == "海西州" ? "海西" : city
+        name = city == "黄南州" ? "黄南" : city
+        name = city == "玉树州" ? "玉树" : city
+        name = city == "果洛州" ? "果洛" : city
+        name = city == "甘南州" ? "甘南" : city
+        name = city == "伊犁州" ? "伊犁" : city
+        name = city == "兴安盟乌兰浩特" ? "兴安盟" : city
+        name = city == "德宏州" ? "德宏" : city
+        name = city == "文山州" ? "文山" : city
+        name = city == "楚雄州" ? "楚雄" : city
+        name = city == "西双版纳州" ? "西双版纳" : city
+        name = city == "昌江县" ? "昌江" : city
+        name = city == "琼中县" ? "琼中" : city
+        name = city == "保亭县" ? "保亭" : city
+        name = city == "陵水县" ? "陵水" : city
+        return name
+    }
+
+    // 取得城市显示颜色
+    var getColorByCity = function(city, province) {
+        var nCount
+        var cities = province.children
+        if(isMunicipality(city))
+            nCount = province['total']['confirm']
+        else {
+            for (var i = cities.length - 1; i >= 0; i--) {
+                var name = correctCityName(cities[i].name)
+                if (city.indexOf(name) != -1) {
+                    nCount = cities[i]['total']['confirm']
+                }
+            }
+        }
+        return getColor(nCount)
+    }
+
+    // 显示省级图层
+    var showProvince = function(adcode, pro) {
         var pros = china.children
-        var cities
+        var province
         for (var i = pros.length - 1; i >= 0; i--) {
-            if (province.indexOf(pros[i].name) != -1) {
-                cities = pros[i].children
+            if (pro.indexOf(pros[i].name) != -1) {
+                province = pros[i]
                 break;
             }
         }
@@ -92,7 +120,9 @@ $(function(){
             depth: 1,
             styles: {
                 'fill': function (props) {
-                    return getColorByCity(props.NAME_CHN, cities);
+                    //console.log(props.NAME_CHN)
+                    //console.log(province.children)
+                    return getColorByCity(props.NAME_CHN, province);
                 },
                 'province-stroke': 'white',
                 'city-stroke': 'blue', // 中国地级市边界
@@ -102,6 +132,7 @@ $(function(){
         updateProvince.setMap(map)
     }
 
+    // 初始全国图层
     var initCountry = new AMap.DistrictLayer.Country({
         zIndex:10,
         SOC:'CHN',
@@ -117,6 +148,7 @@ $(function(){
         }
     })
 
+    // 更新全国图层显示
     var updateCountry = new AMap.DistrictLayer.Country({
         zIndex:10,
         SOC:'CHN',
@@ -139,8 +171,8 @@ $(function(){
             zooms: [3, 10],
             showIndoorMap:false,
             zoom: 3,
-            isHotspot:false,
-            touchZoomCenter:1,
+            //isHotspot:false,
+            //touchZoomCenter:1,
             pitch: 0,
             layers:[
                 initCountry,
@@ -148,7 +180,7 @@ $(function(){
             ],
             viewMode:'2D',
             resizeEnable: true,
-            center: [105.00,32.0]
+            center: [105.00,32.0],
     })
     map.addControl(new AMap.Scale());
     map.on('click',function(e){
@@ -164,6 +196,13 @@ $(function(){
             }
         });
     })
+
+    AMap.plugin("AMap.DistrictSearch", function() {
+        var districtSearch = new AMap.DistrictSearch({
+          level: "district",
+          subdistrict: 2
+        });
+    });
 
     // 从腾讯平台获取数据
     $.ajax({
